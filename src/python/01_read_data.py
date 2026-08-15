bc0fe375f350bcd5e81e507bf1cdbc2eedbba6e0
MonicaNkele <daniellemonica26@gmail.com>
2026-08-15 12:01:16 +0200

    Add script to read .sav file and convert to CSV
    This script reads a .sav file, converts it to a pandas DataFrame, and saves it as a CSV file. It also prints metadata and DataFrame details.

---
+++ src/python/01_read_data.py
@@ -0,0 +1,25 @@
+# Importer les bibliothèques
+import pyreadstat
+
+# Lire le fichier .sav et le convertir en DataFrame pandas
+df, meta = pyreadstat.read_sav('Student-Math-Performance-by-Teaching-Style/data/raw/ProjectData.sav')
+
+# Convertir le DataFrame en fichier CSV
+df.to_csv('Student-Math-Performance-by-Teaching-Style/data/raw/ProjectData.csv', index=False)
+
+# Afficher les métadonnées du fichier .sav
+print(meta.column_names_to_labels)
+
+# Afficher les étiquettes de valeurs des variables
+print(meta.variable_value_labels)
+
+# Afficher les premières lignes
+print(df.head())
+
+# Afficher le nombre de lignes et de colonnes
+print(meta.number_rows)
+
+print(meta.number_columns)
+
+# Afficher les types de données des colonnes
+print(df.dtypes)
