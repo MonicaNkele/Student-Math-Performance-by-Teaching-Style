9ca9ac5cea58a1fb72bfe7b01ebcffb6ff66f61c
MonicaNkele <daniellemonica26@gmail.com>
2026-08-15 12:06:03 +0200

    Add script to read .sav file and export to .csv
    This script reads a .sav file into a DataFrame and exports it as a .csv. It also displays metadata, the first few rows, and the dimensions of the DataFrame.

---
+++ src/r/01_read_data.R
@@ -0,0 +1,20 @@
+# Importer les bibliothèques
+library(tidyverse)
+library(haven)
+
+# Lire le fichier .sav et exporter en DataFrame .csv
+df <- read_sav("/Users/monica/Desktop/math-score/data/raw/ProjectData.sav")
+write_csv(df, "/Users/monica/Desktop/math-score/data/raw/NewProjectData.csv")
+
+# Afficher les métadonnées du fichier .sav
+attributes(df)
+
+# Afficher les premières lignes
+head(df)
+
+# Afficher le nombre de lignes et de colonnes
+nrow(df)
+ncol(df)
+
+# Afficher les types de données des colonnes
+vapply(df, typeof, character(1))
